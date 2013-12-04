28d3ae810960854aecfc996d1d9e014843f8ce75
jforjava1981 <jforjava1981@gmail.com>
2013-12-04 16:27:02 +0530

    tt

---
+++ Test/com/GitTest.java
@@ -0,0 +1,9 @@
+package com;
+/* this is my first file creted using GitHub UI
+ Trying to add a new file to my GitHub repository and then synch the same 
+ with my local repo cloned from this repository. */
+ public class GitTest{
+        public static void main(String args[]){
+          System.out.println("In first GitHub hosted java program");
+        }|
+ }
